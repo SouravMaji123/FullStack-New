61fd7c38e5bd9aea1ece2515c6322cc37b1bf1e8
SouravMaji123 <sourv.maji.05@gmail.com>
2024-12-20 01:57:26 +0530

    add vercel

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AddJob, JobDetail, JobList, Login, Register, NotFound } from "../pages/index";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<JobList />} />
+        <Route path="/addjob" element={<AddJob />} />
+        <Route path="/editjob/:id" element={<AddJob />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/list/:id" element={<JobDetail />} />
+        {/* // TODO: MAKE IT NESTED WHEN ADDING NAVBAR */}
+        <Route path="/list" element={<JobList />}>
+          <Route index element={<JobList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
